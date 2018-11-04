9cc7203d82b2ebe04a0885cc9e56f8f1bb2a0830
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-11-04 23:58:40 +0100

    Update AddingMultiplePoints.py

--- AddingMultiplePoints.py
+++ AddingMultiplePoints.py
@@ -5,3 +5,4 @@ for coordinates in [[38,2,-99,1],[39,2,-97,1]]:
 fg.add_child(folium.Marker(location = coordinates,popup = "Hi I am a Marker", icon = folium.Icon(color='green')))
 map.add_child(fg)
 map.save("Map1.html")
+#we can also edit 
